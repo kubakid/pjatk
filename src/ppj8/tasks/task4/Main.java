59acf5c6777d95caa53a4d2630fafa4710ca6f3c
JK <kuba.kidon@wp.pl>
2022-12-06 18:28:08 +0100

    $ git config --global user.name "JK"

--- src/ppj8/tasks/task4/Main.java
+++ src/ppj8/tasks/task4/Main.java
@@ -8,37 +8,51 @@ public class Main {
                 {9, 10, 11, 12},
                 {13, 14, 15, 16}
         };
-
         int[][] vectors = {
                 {1, 0},
-                {0,-1},
-                {-1,0},
-                {0, 1}
+                {0, 1},
+                {-1, 0},
+                {0, -1}
         };
-
         int x = 0;
-        int y= 0;
+        int y = 0;
         int vec = 0;
 
-        int [] arr = new int[tab.length * tab[0].length];
+        int[] arr = new int[tab.length * tab[0].length];
 
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length-1; i++) {
             boolean duplicated = false;
-            for (int j = 0; j < arr.length; j++){ // sprawdzenie czy nie zrobi się po prostu kwadrat
-                if(tab[y+ vectors[vec][1]][x + vectors[vec][0]] == arr[j]){
-                    duplicated = true;
+            boolean isOutOfRange =false;
+            arr[i] = tab[y][x];
+
+            if(x + vectors[vec][0] > tab.length - 1 || y + vectors[vec][1] > tab.length - 1 || x + vectors[vec][0] < 0 || y + vectors[vec][1] < 0){
+                isOutOfRange = true;
+            }
+            for (int j = 0; j <= i; j++) {
+                if(isOutOfRange){
                     break;
+                }else {
+                    if (tab[y + vectors[vec][1]][x + vectors[vec][0]] == arr[j]) {
+                        duplicated = true;
+                        break;
+                    }
                 }
-
             }
-            arr[i] = tab[x][y];
-
-            // w ifie sprawdzamy czy nie wyjdzie poza indeks naszej planszy oraz podpinamy warunek z poprzedniego fora
-
-            if ( x + vectors[vec][0] >= 0 && x + vectors[vec][0] <= tab.length-1 && y+ vectors[vec][1] >= 0 && y + vectors[vec][1] <= tab.length-1 && !duplicated){
-                x += vectors[vec][0];
-                y += vectors[vec][1];
+            if (isOutOfRange || duplicated) {
+                if (vec == 3)
+                    vec = 0;
+                else
+                    vec+=1;
             }
+
+            x += vectors[vec][0];
+            y += vectors[vec][1];
+        }
+
+        arr[tab.length * tab[0].length - 1] = tab[y][x];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
         }
     }
 }
